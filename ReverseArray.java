6fe9607e22236b1caf4dd4634b73f94865aa201f
roopa2012 <rooparam5389367@gmail.com>
2026-06-13 12:27:02 +0530

    Create ReverseArray.java

---
+++ ReverseArray.java
@@ -0,0 +1,11 @@
+public class ReverseArray{
+  public static void main(String[] args){
+    int[] num = { 1,2,3,4,5,6,7,87,9,20};
+    
+    for(int i = num.length-1 ;i>=0 ; i--){
+     
+    
+    System.out.println(num[i]);
+    }
+  }
+}
