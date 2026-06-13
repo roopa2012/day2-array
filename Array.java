c38b1455159d38c3c7d11ca2ffb3298255b41315
roopa2012 <rooparam5389367@gmail.com>
2026-06-13 11:59:22 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array{
+  public static void main(String[] args){
+    int[] names = { roopa ,keerthi,fasi,indhu,charchi,priyanka};
+    for(int i=0,i<arr.length,i++){
+      System.out.println(arr[i]);
+    }
+  }
+}
