07cfaf6c2a411043150d7264dcf3051dcad4c693
roopa2012 <rooparam5389367@gmail.com>
2026-06-13 12:06:15 +0530

    Create SumArray.java

---
+++ SumArray.java
@@ -0,0 +1,13 @@
+public class SumArray{
+  public static void main(String[]args){
+    int[] num={2,3,4,5,6,7,8,9};
+    int sum = 0;
+    for(int i = 0 ; i <num.length;i++){
+      sum+=num[i];
+    }
+      System.out.println("sum"+ "="+sum);
+    
+  }
+}
+
+      
